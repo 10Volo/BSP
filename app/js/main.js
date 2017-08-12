f33980e614a90886613324387014769aca3712a2
10Volo <volodymyr.fenyak@gmail.com>
2017-08-12 15:33:48 +0300

    production

--- app/js/main.js
+++ app/js/main.js
@@ -7,10 +7,45 @@ function menu_toggle(header) {
     header.classList.toggle('show');
 }
 
+function validation() {
+    var contactname=document.enq.name.value;
+    var name_exp=/^[A-Za-z\s]+$/;
+    if(contactname=='')
+    {
+        alert("Name Field Should Not Be Empty!");
+        document.enq.name.focus();
+        return false;
+    }
+    else if(!contactname.match(name_exp))
+    {
+        alert("Invalid Name field!");
+        document.enq.name.focus();
+        return false;
+    }
 
-// button call
+    var email=document.enq.email.value;
+    //var email_exp=/^[A-Za-z0-9\.-_\$]+@[A-Za-z]+\.[a-z]{2,4}$/;
+    var email_exp=/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    if(email=='')
+    {
+        alert("Please Enter Email-Id!");
+        document.enq.email.focus();
+        return false;
+    }
+    else if(!email.match(email_exp))
+    {
+        alert("Invalid Email ID !");
+        document.enq.email.focus();
+        return false;
+    }
 
-$("#call").on('click', function() {
-    var link = "tel:18003334444";
-    window.location.href = link;
-});
+
+    var message=document.enq.message.value;
+    if(message=='')
+    {
+        alert("Query Field Should Not Be Empty!");
+        document.enq.message.focus();
+        return false;
+    }
+    return true;
+}
